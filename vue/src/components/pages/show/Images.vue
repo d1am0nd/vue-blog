632aee83b83cf0c0b4bec6b8b340ec14195eead8
devk <devkordesg@gmail.com>
2017-05-28 22:13:29 +0200

    simple search added

--- vue/src/components/pages/show/Images.vue
+++ vue/src/components/pages/show/Images.vue
@@ -2,8 +2,19 @@
   <div class="images">
     <h1>Hip Hop Blog Images</h1>
 
-    <div class="row" v-for="image in images">
+    <div class="row">
       <div class="col-md-12">
+        <input
+          type="text"
+          placeholder="Filter"
+          v-model="filter">
+        </input>
+      </div>
+    </div>
+
+    <div class="row" v-for="image in filteredImages">
+      <div class="col-md-12">
+        {{ image.name }} <br>
         <router-link :to="{ name: 'editImage', params: { id: image.id } }">
           <img :src="image.path">
         </router-link>
@@ -20,11 +31,21 @@ export default {
   name: 'images',
   data () {
     return {
-      images: []
+      images: [],
+      filter: ''
     }
   },
   created () {
     this.fetchData()
+  },
+  computed: {
+    filteredImages () {
+      var vm = this
+      return this.images.filter((item) => {
+        console.log(item)
+        return item.name.indexOf(vm.filter) !== -1
+      })
+    }
   },
   methods: {
     fetchData () {
